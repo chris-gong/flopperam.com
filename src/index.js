804c456cd289602761f43adc2f77a31d3c4d1dc2
BEAST <49040508+BEASTSM96@users.noreply.github.com>
2020-09-12 21:00:14 +0100

    Added Project folders  (#1)
    * Added folders to make it more clean
    
    * Added folders to make it more clean
    
    * moved index.js to src folder since react requires that for some reason and fixed some import statements
    
    Co-authored-by: chris-gong <chris_gong@yahoo.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './css/index.css';
+import App from './js/App';
+import * as serviceWorker from './js/serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
